9ed6a2f88269870207ab672ceb4d396b3c240080
upasanab7 <40449364+upasanab7@users.noreply.github.com>
2018-06-21 18:27:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/upasana/test/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.upasana.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mDriver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDriver= (Button) findViewById(R.id.driver);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this, maptest.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
